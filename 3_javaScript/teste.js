b9b4ee5684eb77f98c129ad0e3ab38aa053c0993
Renanfls <contato.renanfabricio@gmail.com>
2023-05-25 13:01:34 -0300

    Init JS

---
+++ 3_javaScript/teste.js
@@ -0,0 +1,35 @@
+console.log("olá");
+console.log();
+
+var a = 10;
+var b = 20;
+var total = a + b;
+console.log("total: " + total);
+console.log();
+
+var nomePessoa = "Lucas";
+var NomePessoa = "Jose";
+console.log("nomePessoa: " + nomePessoa);
+console.log("NomePessoa: " + NomePessoa);
+console.log();
+
+var var1 = 1, 
+    var2 = 2, 
+    var3 = 3;
+console.log("var3: " + var3);
+console.log();
+
+var n1; // Se não for atribuir valor inicial
+n1 = 20;
+console.log("n1: " + n1);
+console.log();
+
+let book = 10; // Se for atribuir valor inicial
+book = 20;
+console.log("book: " + book);
+console.log();
+
+const valor1 = 100; // Não pode ser alterado
+// valor1 = 200; // Dá erro
+console.log("valor1: " + valor1);
+console.log();
